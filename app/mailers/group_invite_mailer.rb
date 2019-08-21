4ff6a4d569016dcb96d15e06efd5e15124a7ca7d
gwalshington <walshcostigan@gmail.com>
2019-08-21 17:14:13 -0400

    mailers and dashboard updates

--- app/mailers/group_invite_mailer.rb
+++ app/mailers/group_invite_mailer.rb
@@ -3,7 +3,7 @@ class GroupInviteMailer < ApplicationMailer
 
   def send_invite(id)
     @group_invite = GroupInvite.find(id)
-
+    @referral_link = 'https://www.caresplit.com/users/sign_up/?' + @group_invite.email + ''
     @subject = 'You are invited to join ' + @group_invite.group.name + ' by ' + @group_invite.invitee.first_name + ''
     mail(to: @group_invite.email, subject: @subject)
   end
---
+++ app/mailers/group_mailer.rb
@@ -0,0 +1,10 @@
+class GroupMailer < ApplicationMailer
+  default from: "hello@caresplit.com"
+
+  def confirm_new_group(id, user_id)
+    @group = Group.find(id)
+    @user = User.find(id)
+    @subject = 'You created a new Split group - ' + @group_invite.group.name + ''
+    mail(to: @user.email, subject: @subject)
+  end
+end
